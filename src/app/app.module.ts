2117f5fde7afe8b0d9c253f5237b863dc2d015af
Gabriel Fernandes Alencar <gfalencar02@gmail.com>
2022-04-22 11:23:51 -0300

    Trabalhando com rotas simples , routerLink, e CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { FormsModule } from '@angular/forms';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
