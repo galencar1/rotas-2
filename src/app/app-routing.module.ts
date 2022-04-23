d7d0aad19e64b58efaf3b500d34fe2698a4ea161
Gabriel Fernandes Alencar <gfalencar02@gmail.com>
2022-04-23 15:35:47 -0300

    Criando um módulo de rotas de funcionalidade!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,22 @@
 import { NgModule } from '@angular/core';
-import { ModuleWithProviders } from "@angular/core";
+//import { ModuleWithProviders } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 //import { RouterModule, Routes } from '@angular/router';
 
 //const routes: Routes = [];
 
 
-import { CursoDetalheComponent } from "./cursos/curso-detalhe/curso-detalhe.component";
-import { CursoNaoEncontradoComponent } from "./cursos/curso-nao-encontrado/curso-nao-encontrado.component";
-import { CursosComponent } from "./cursos/cursos.component";
+//import { CursoDetalheComponent } from "./cursos/curso-detalhe/curso-detalhe.component";
+//import { CursoNaoEncontradoComponent } from "./cursos/curso-nao-encontrado/curso-nao-encontrado.component";
+//import { CursosComponent } from "./cursos/cursos.component";
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
 
 const appRoutes: Routes = [
-    {path: 'cursos', component: CursosComponent},
-    {path: 'curso/:id', component: CursoDetalheComponent},
+    //{path: 'cursos', component: CursosComponent},
+    //{path: 'curso/:id', component: CursoDetalheComponent},
     {path: 'login', component: LoginComponent},
-    {path: 'naoEncontrado', component: CursoNaoEncontradoComponent},
+    //{path: 'naoEncontrado', component: CursoNaoEncontradoComponent},
     {path: '', component: HomeComponent}
 
 ];
