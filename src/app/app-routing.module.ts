a32c2afb3d5d22bf47fe13445ec94791a5709d67
Gabriel Fernandes Alencar <gfalencar02@gmail.com>
2022-04-23 15:05:55 -0300

    Criando um módulo de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { ModuleWithProviders } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+//import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//const routes: Routes = [];
+
+
+import { CursoDetalheComponent } from "./curso-detalhe/curso-detalhe.component";
+import { CursoNaoEncontradoComponent } from "./curso-nao-encontrado/curso-nao-encontrado.component";
+import { CursosComponent } from "./cursos/cursos.component";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
+
+const appRoutes: Routes = [
+    {path: 'cursos', component: CursosComponent},
+    {path: 'curso/:id', component: CursoDetalheComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'naoEncontrado', component: CursoNaoEncontradoComponent},
+    {path: '', component: HomeComponent}
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
 
-import { routing } from './app.routing';
+//import { routing } from './app.routing';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursosService } from './cursos/cursos.service';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
@@ -26,7 +26,8 @@ import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-en
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    routing
+    AppRoutingModule
+    //routing
   ],
   providers: [CursosService],
   bootstrap: [AppComponent]
